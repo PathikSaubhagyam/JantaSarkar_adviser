24f80890cc76e0167034ea964da32aad9a77d1c2
Pathik Panchal <pathik.saubhagyam@gmail.com>
2026-03-30 14:23:26 +0530

    ios set up

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ import App from './App';
 import { name as appName } from './app.json';
 
 // Register background handler for FCM
-messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log('Background Message received in index.js:', remoteMessage);
-});
+try {
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Background Message received in index.js:', remoteMessage);
+  });
+} catch (e) {
+  console.warn('Firebase messaging not available:', e);
+}
 
 AppRegistry.registerComponent(appName, () => App);
